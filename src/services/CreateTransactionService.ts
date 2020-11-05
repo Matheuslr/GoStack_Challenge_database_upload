4cd07856f8765498b6915fdf59faf58c2c4a6f0a
Matheus Lucas Silva Meirelles Reis <matheus.reis@polsec.ai>
2020-11-05 01:46:46 -0300

    add transactions CRUD

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface RequestDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category_title: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    category_title,
+    title,
+    type,
+    value,
+  }: RequestDTO): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Outcome cannot be bigger than total balance');
+    }
+
+    let category = await categoryRepository.findOne({
+      where: { title: category_title },
+    });
+
+    if (!category) {
+      category = categoryRepository.create({ title: category_title });
+      await categoryRepository.save(category);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+      category_id: category.id,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
